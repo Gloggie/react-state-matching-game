cf1c9cd38e277782c23baeb495164413a20438e6
Peter Svensson <peter.z.svensson@teliacompany.com>
2021-09-21 13:04:59 +0200

    create refactor dir

---
+++ refactor/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+const Button = (props) => (<button onClick={props.startGame}>
+    {
+        props.playing
+            ? 'reset'
+            : 'start'
+    }
+</button>)
+
+export default Button
